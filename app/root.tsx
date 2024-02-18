9d38013f7efebbe67261fcef36c3720022115dba
Qz <qzcurious@gmail.com>
2024-02-18 10:38:53 +0800

    setup tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -1,10 +1,36 @@
+import { LinksFunction } from "@remix-run/node";
 import {
   Links,
   Meta,
+  MetaFunction,
   Outlet,
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import "./tailwind.css";
+
+export const links: LinksFunction = () => [
+  { rel: "preconnect", href: "https://fonts.googleapis.com" },
+  {
+    rel: "preconnect",
+    href: "https://fonts.gstatic.com",
+    crossOrigin: "anonymous",
+  },
+  {
+    rel: "stylesheet",
+    href: "https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap",
+  },
+];
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Simple Todo List" },
+    {
+      name: "description",
+      content: "Todo List with Remix, Design by 黃奇昌",
+    },
+  ];
+};
 
 export default function App() {
   return (
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,16 +1,7 @@
-import type { MetaFunction } from "@remix-run/node";
-
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix SPA" },
-    { name: "description", content: "Welcome to Remix (SPA Mode)!" },
-  ];
-};
-
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix (SPA Mode)</h1>
+      <h1 className="font-bold text-6xl">Do it Now</h1>
       <ul>
         <li>
           <a
